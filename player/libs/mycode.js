5aa4039a81b397b517fee7f3e545e123b59dcf72
john conor <conor.john.1969@gmail.com>
2016-02-16 16:26:39 +0600

    add jquery

--- player/libs/mycode.js
+++ player/libs/mycode.js
@@ -2,6 +2,25 @@ window.onload = function(){
 
 }
 
+window.onkeyup = function(event) {
+  switchbar();
+}
+
+
+
+var showbar = 0;
+function switchbar() {
+  if (showbar == 0) {
+    $("#PlaybackControl").toggle();
+    showbar = 1;
+  }
+
+  else {
+    $("#PlaybackControl").hide();
+    showbar = 0
+  }
+  
+};
 function renderTime() {
 
 
@@ -10,7 +29,10 @@ var h = timenow.getHours();
 var m = timenow.getMinutes();
 
 var myTime = document.getElementById('time');
+if (m < 10) {
+m = "0" + m
+};
 myTime.innerHTML = h + ":" + m 
-setTimeout('renderTime()', 3000);
+setTimeout('renderTime()', 15000);
 }
 renderTime();
